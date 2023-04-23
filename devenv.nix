2a2f4bdc81a3a4cbc7f5077927b6536b4dd9ed7a
Alessandro Candido <candido.ale@gmail.com>
2023-04-24 01:27:24 +0200

    Add pre-commit hooks

--- devenv.nix
+++ devenv.nix
@@ -1,22 +1,11 @@
-{ pkgs, ... }:
-
 {
-  # https://devenv.sh/packages/
-  packages = with pkgs; [ git ];
-
-  # https://devenv.sh/scripts/
   scripts.fmt.exec = "nix fmt .";
 
-  enterShell = "";
+  languages.nix.enable = true;
 
-  # https://devenv.sh/languages/
-  # languages.nix.enable = true;
-
-  # https://devenv.sh/pre-commit-hooks/
-  # pre-commit.hooks.shellcheck.enable = true;
-
-  # https://devenv.sh/processes/
-  # processes.ping.exec = "ping example.com";
-
-  # See full reference at https://devenv.sh/reference/options/
+  pre-commit.hooks = {
+    deadnix.enable = true;
+    markdownlint.enable = true;
+    nixpkgs-fmt.enable = true;
+  };
 }
