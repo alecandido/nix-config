afb6b0c47d509effd39b1fca00761806a1898524
Alessandro Candido <candido.ale@gmail.com>
2023-04-23 18:06:51 +0200

    Restore nixpkgs master as unique provider

--- flake.nix
+++ flake.nix
@@ -4,14 +4,12 @@
   inputs = {
     # Package sets
     nixpkgs.url = "github:nixos/nixpkgs";
-    nixpkgs-darwin.url = "github:nixos/nixpkgs/nixpkgs-22.11-darwin";
 
     # Environment/system management
     darwin.url = "github:lnl7/nix-darwin";
-    darwin.inputs.nixpkgs.follows = "nixpkgs-darwin";
+    darwin.inputs.nixpkgs.follows = "nixpkgs";
     home-manager.url = "github:nix-community/home-manager";
-    # TODO: make system dependent
-    home-manager.inputs.nixpkgs.follows = "nixpkgs-darwin";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
     # Tools
     devenv.url = "github:cachix/devenv/latest";
