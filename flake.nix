9ccdf416f349bbdf17bd0a4d434b9e15f288f3b0
Alessandro Candido <candido.ale@gmail.com>
2023-04-23 17:48:39 +0200

    Add nixpkgs-darwin aside to nixpkgs

--- flake.nix
+++ flake.nix
@@ -4,12 +4,14 @@
   inputs = {
     # Package sets
     nixpkgs.url = "github:nixos/nixpkgs";
+    nixpkgs-darwin.url = "github:nixos/nixpkgs/nixpkgs-22.11-darwin";
 
     # Environment/system management
     darwin.url = "github:lnl7/nix-darwin";
-    darwin.inputs.nixpkgs.follows = "nixpkgs";
+    darwin.inputs.nixpkgs.follows = "nixpkgs-darwin";
     home-manager.url = "github:nix-community/home-manager";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    # TODO: make system dependent
+    home-manager.inputs.nixpkgs.follows = "nixpkgs-darwin";
 
     # Tools
     devenv.url = "github:cachix/devenv/latest";
