99526175c2db00481ad7b9f6e33f42f60ca62738
Alessandro Candido <candido.ale@gmail.com>
2023-04-25 11:40:40 +0200

    Pin nixpkgs revision to the one used while building

--- top/nix.nix
+++ top/nix.nix
@@ -1,8 +1,10 @@
-{ nixpkgs, flake-utils, ... }@inputs:
+{ nixpkgs, flake-utils, ... }:
 
 let
   eachSystem = flake-utils.lib.eachDefaultSystem;
 in
 (eachSystem (system: {
   formatter = nixpkgs.legacyPackages.${system}.nixpkgs-fmt;
-}))
+})) // {
+  nix.registry.nixpkgs.flake = nixpkgs;
+}
