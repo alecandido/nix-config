c3d3c7f4a13add1117267b8b7e430c0e4cc4fa68
Alessandro Candido <candido.ale@gmail.com>
2023-08-22 18:49:53 +0200

    Close the submodule epos

--- home/aliases/default.nix
+++ home/aliases/default.nix
@@ -2,7 +2,8 @@
   home.shellAliases = {
     ls = "exa";
     nv = "nvim";
-    upgrade =
-      "darwin-rebuild switch --flake $XDG_CONFIG_HOME/nixpkgs --show-trace";
+    upgrade = ''
+        darwin-rebuild switch --flake "$XDG_CONFIG_HOME/nixpkgs?submodules=1" --show-trace
+      '';
   };
 }
