ee848a6ca2672247dc7428fc609ce4e422ae7cf8
Alessandro Candido <candido.ale@gmail.com>
2023-08-22 18:15:00 +0200

    Determine correct server packages from mason.nvim registry https://mason-registry.dev/registry/list

--- home/neovim/servers.nix
+++ home/neovim/servers.nix
@@ -2,6 +2,12 @@
 
 {
   home.packages = with pkgs; [
+    nil
     lua-language-server
-  ];
+  ] ++ (with pkgs.nodePackages; [
+    typescript-language-server
+    pyright
+  ]) ++ (with pkgs.vscode-extensions; [ 
+    dbaeumer.vscode-eslint 
+  ]);
 }
