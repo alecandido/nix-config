0263b1442e8952350b44f00c7701238b99fa1f45
Alessandro Candido <candido.ale@gmail.com>
2023-08-22 17:15:39 +0200

    Fix bug in bindings definition

--- home/neovim/lua/plugins/language/lsp-zero.lua
+++ home/neovim/lua/plugins/language/lsp-zero.lua
@@ -27,8 +27,8 @@ local function on_attach(_, bufnr)
   nmap('<leader>td', vim.lsp.buf.type_definition, 'Type [D]efinition')
   nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
   nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
-  nmap('[d', vim.diagnostic.goto_next(), 'Previous [D]iagnostic')
-  nmap(']d', vim.diagnostic.goto_prev(), 'Next [D]iagnostic')
+  nmap('[d', vim.diagnostic.goto_next, 'Previous [D]iagnostic')
+  nmap(']d', vim.diagnostic.goto_prev, 'Next [D]iagnostic')
 
   -- Lesser used LSP functionality
   nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
