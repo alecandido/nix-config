e0fe3180301c7b779888057f1437b4b8d3dff6ae
Alessandro Candido <candido.ale@gmail.com>
2023-08-24 12:17:51 +0200

    Add further programs for more previews

--- home/amenities.nix
+++ home/amenities.nix
@@ -3,6 +3,15 @@
 {
   home.packages = with pkgs;
   [
+    # manipulate images
+    imagemagick
+    # render images in browser
     chafa
+    # render PDF to many formats
+    poppler_utils
+    # render video thumbnails
+    ffmpegthumbnailer
+    # preview fonts in terminal
+    fontpreview
   ];
 }
