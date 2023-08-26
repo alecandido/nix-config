838e99402b83ca1e290297b8f1304e02e5189686
Alessandro Candido <candido.ale@gmail.com>
2023-08-27 01:58:15 +0200

    Trust devenv substituter

--- etc/nix.nix
+++ etc/nix.nix
@@ -3,10 +3,12 @@
 
   nix.settings = {
     substituters = ["https://cache.nixos.org" "https://alecandido.cachix.org"];
+    trusted-substituters = ["https://devenv.cachix.org"];
     trusted-users = ["root"];
     trusted-public-keys = [
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       "alecandido.cachix.org-1:TgsrZYrtoZVpZ9nSlYBw2ylVFpHqLI1YnwhPTwjGdgM="
+      "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
     ];
   };
 }
