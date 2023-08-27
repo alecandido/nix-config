6f3c9dd9cd77875cdcf0344ad43b052f6c826ddd
Alessandro Candido <candido.ale@gmail.com>
2023-08-27 10:30:00 +0200

    Add formatters

--- home/neovim/formatters.nix
+++ home/neovim/formatters.nix
@@ -1,7 +1,22 @@
 {pkgs, ...}: {
-  home.packages = with pkgs; [
-    stylua
-    alejandra
-    fnlfmt
-  ];
+  home.packages = with pkgs;
+    [
+      alejandra
+      black
+      fnlfmt
+      isort
+      ktlint
+      pgformatter
+      shfmt
+      stylish-haskell
+      stylua
+      taplo
+      uncrustify
+    ]
+    ++ (with pkgs.nodePackages; [
+      prettier
+    ])
+    ++ (with pkgs.python311Packages; [
+      docformatter
+    ]);
 }
