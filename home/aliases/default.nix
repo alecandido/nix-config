b93fd9a70862311f7b4a814e4f57e2efbd290636
Alessandro Candido <candido.ale@gmail.com>
2023-11-04 12:25:17 +0100

    Introduce common aliases

--- home/aliases/default.nix
+++ home/aliases/default.nix
@@ -1,7 +1,11 @@
 {
   home.shellAliases = {
     ls = "exa";
+    la = "ls -a";
+    ll = "ls -l";
     nv = "nvim";
+    py = "python";
+    ipy = "ipython";
     upgrade = ''
       darwin-rebuild switch --flake "$XDG_CONFIG_HOME/nixpkgs?submodules=1" --show-trace
     '';
