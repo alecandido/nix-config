3ea991748ad46b3d7ae691b1c55059a2ba96ed8c
Alessandro Candido <candido.ale@gmail.com>
2023-11-14 13:13:42 +0100

    Start developing neovim toggle

--- home/neovim/default.nix
+++ home/neovim/default.nix
@@ -1,13 +1,21 @@
-{ pkgs, ... }: {
-  # imports = [
-  #   ./linters
-  #   ./formatters.nix
-  #   ./servers.nix
-  # ];
-  #
-  # home.packages = with pkgs; [
-  #   tree-sitter
-  # ];
+{ pkgs, ... }: 
+let 
+  lsp = builtins.elem "neovim.lsp" []; #toggles;
+
+  imports = (if lsp
+	  then [
+	   ./linters
+	   ./formatters.nix
+	   ./servers.nix
+	  ] else []);
+  
+  hpkgs = (if lsp then (with pkgs; [
+    tree-sitter
+  ]) else []);
+in {
+  inherit imports;
+
+  home.packages = hpkgs;
 
   programs.neovim.enable = true;
   programs.neovim.defaultEditor = true;
