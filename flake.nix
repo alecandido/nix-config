e23b7f113d84e55a1bad0e5e4c7c7436c376f24b
Alessandro Candido <candido.ale@gmail.com>
2023-11-14 13:15:10 +0100

    Integrate current lxplus configurations

--- flake.nix
+++ flake.nix
@@ -28,3 +28,13 @@
     inherit (nixConfigs) formatter;
   };
 }
+
+#  scripts.format.exec = "nix fmt .";
+#
+#  languages.nix.enable = true;
+#
+#  pre-commit.hooks = {
+#    deadnix.enable = true;
+#    markdownlint.enable = true;
+#    nixpkgs-fmt.enable = true;
+#  };
