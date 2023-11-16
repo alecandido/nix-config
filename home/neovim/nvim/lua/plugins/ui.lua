9c8e4c80f8ec928670f4694402b012ce4f6f0a9d
Alessandro Candido <candido.ale@gmail.com>
2023-11-16 23:10:07 +0100

    Drop indent-blankline conifgs

--- home/neovim/nvim/lua/plugins/ui.lua
+++ home/neovim/nvim/lua/plugins/ui.lua
@@ -27,13 +27,7 @@ return {
   },
 
   -- Add indentation guides even on blank lines
-  {
-    "lukas-reineke/indent-blankline.nvim",
-    opts = {
-      show_current_context = true,
-      show_trailing_blankline_indent = false,
-    },
-  },
+  { "lukas-reineke/indent-blankline.nvim" },
 
   {
     "lukas-reineke/headlines.nvim",
