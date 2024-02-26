b76da8aeaf8e0af34bbd5c6ba21ac88d7500527b
Alessandro Candido <candido.ale@gmail.com>
2024-02-26 10:55:37 +0100

    Add python development shell with associated virtual environment

--- shells/default.nix
+++ shells/default.nix
@@ -10,8 +10,15 @@ forEachSystem
   (system:
   let
     pkgs = nixpkgs.legacyPackages.${system};
+    devenvDisclaimer = ''
+      devenv_dir=$PWD/.devenv
+      if [ ! -d $devenv_dir ]; then
+        echo "Creating devenv dir at '$devenv_dir'"
+      fi
+    '';
   in
   {
+    # a shell for this repo
     default = devenv.lib.mkShell {
       inherit inputs pkgs;
       modules = [
@@ -28,12 +35,32 @@ forEachSystem
       ];
     };
 
+    # make python available
     python = devenv.lib.mkShell {
       inherit inputs pkgs;
 
       modules = [
         {
+          enterShell = devenvDisclaimer;
+
           languages.python.enable = true;
+        }
+      ];
+    };
+
+    # make python available
+    # with a related virtual environment, to install dependencies
+    pyvenv = devenv.lib.mkShell {
+      inherit inputs pkgs;
+
+      modules = [
+        {
+          enterShell = devenvDisclaimer;
+
+          languages.python = {
+            enable = true;
+            venv.enable = true;
+          };
         }
       ];
     };
