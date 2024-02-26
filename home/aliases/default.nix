870d788f3f6e785ebf61a32c2f1a5a84e60d7ff7
Alessandro Candido <candido.ale@gmail.com>
2024-02-26 09:47:36 +0100

    Add develop alias for global shell enabling

--- home/aliases/default.nix
+++ home/aliases/default.nix
@@ -1,3 +1,7 @@
+{ config, ... }:
+let
+  xdg = config.xdg;
+in
 {
   home.shellAliases = {
     ls = "eza";
@@ -7,6 +11,7 @@
     py = "python";
     ipy = "ipython";
     rename = "rnr";
+    develop = "nix develop ${xdg.configHome}/nixpkgs -c $SHELL";
     dev-flake = ''
       nix flake init --template github:cachix/devenv
     '';
