145da7c10bcc00ff7521cf4240bb850b9efa6428
Alessandro Candido <candido.ale@gmail.com>
2024-03-07 13:30:17 +0100

    refactor: Polish inputs style

--- flake.nix
+++ flake.nix
@@ -6,10 +6,14 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
 
     # Environment/system management
-    darwin.url = "github:lnl7/nix-darwin";
-    darwin.inputs.nixpkgs.follows = "nixpkgs";
-    home-manager.url = "github:nix-community/home-manager";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    darwin = {
+      url = "github:lnl7/nix-darwin";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
     # Flake utilities
     flake-utils.url = "github:numtide/flake-utils";
