3e61e403560c3bb3dc96e5733d70deb9f97d16cc
Alessandro Candido <candido.ale@gmail.com>
2024-03-07 10:53:20 +0100

    fix: Only enable SSH agent on Linux

--- home/ssh/default.nix
+++ home/ssh/default.nix
@@ -1,11 +1,15 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  lib,
+  ...
+}: {
   programs.ssh = {
     enable = true;
     package = pkgs.openssh;
     forwardAgent = true;
   };
 
-  services.ssh-agent.enable = true;
+  services.ssh-agent.enable = lib.strings.hasInfix "linux" pkgs.system;
 
   home.file.".ssh/config".source = ./config;
   home.packages = with pkgs; [sshfs];
