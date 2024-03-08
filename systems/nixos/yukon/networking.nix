df229536aee339576eb25808f400249125efa6ee
Alessandro Candido <candido.ale@gmail.com>
2024-03-08 18:31:31 +0100

    feat: Add systemd service to connect yukon to villarose

--- systems/nixos/yukon/networking.nix
+++ systems/nixos/yukon/networking.nix
@@ -1,10 +1,22 @@
-{
+{pkgs, ...}: {
   networking = {
     nftables.enable = true;
 
     firewall = {
       enable = true;
       allowedTCPPorts = [80 443 8000];
+    };
+  };
+
+  systemd.services.villarose-tunnel = {
+    enable = true;
+    wantedBy = ["multi-user.target"];
+    after = ["network.target" "systemd-networkd-wait-online.service"];
+    description = "Connect to villarose.";
+    script = "${pkgs.openssh}/bin/ssh -N -R 22001:localhost:22 villarose";
+    serviceConfig = {
+      Restart = "on-failure";
+      RestartSec = "5min";
     };
   };
 }
