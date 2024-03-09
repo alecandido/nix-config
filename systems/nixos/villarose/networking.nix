be1495456611603aca85f9fc81705fe9bbb1e31f
Alessandro Candido <candido.ale@gmail.com>
2024-03-09 19:20:26 +0100

    feat: Add ACME authentication for villarose

--- systems/nixos/villarose/networking.nix
+++ systems/nixos/villarose/networking.nix
@@ -29,4 +29,19 @@
     };
     startAt = "*:00/15:00"; # every 15 minutes
   };
+
+  security.acme = {
+    acceptTerms = true;
+    defaults.email = "candido.ale@gmail.com";
+    certs."villarose.annibale.dev" = {
+      dnsProvider = "googledomains";
+      # Supplying password files like this will make your credentials world-readable
+      # in the Nix store. This is for demonstration purpose only, do not use this in production.
+      credentialsFile = let
+        token = config.age.secrets.villarose-https;
+      in "${pkgs.writeText "googledomains-creds" ''
+        GOOGLE_DOMAINS_ACCESS_TOKEN_FILE=${token}
+      ''}";
+    };
+  };
 }
