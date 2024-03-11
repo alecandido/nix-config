2d88250bea0641244e4e264d7ce077ed33c61137
Alessandro Candido <candido.ale@gmail.com>
2024-03-11 15:47:05 +0100

    feat: Enable WOL on klondike

--- systems/nixos/klondike/default.nix
+++ systems/nixos/klondike/default.nix
@@ -3,4 +3,9 @@
     # Include the results of the hardware scan.
     ./hardware-configuration.nix
   ];
+
+  networking.interfaces."eno1".wakeOnLan = {
+    enable = true;
+    policy = ["magic" "phy"];
+  };
 }
