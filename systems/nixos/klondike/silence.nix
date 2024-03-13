862ee1b70c1c5e8718966824611294397cb2171d
Alessandro Candido <candido.ale@gmail.com>
2024-03-13 16:54:00 +0100

    fix: Actually set the shutdown in the silence service

--- systems/nixos/klondike/silence.nix
+++ systems/nixos/klondike/silence.nix
@@ -1,11 +1,10 @@
 # Shut down to avoid a noisy machine running during office hours
-{pkgs, ...}: {
+{
   systemd.services.silence = {
     enable = true;
     description = "Shut down klondike during office hours.";
     serviceConfig = {
-      # ExecStart = "shutdown now";
-      ExecStart = "${pkgs.coreutils}/bin/date";
+      ExecStart = "shutdown now";
     };
     startAt = "08:00";
   };
