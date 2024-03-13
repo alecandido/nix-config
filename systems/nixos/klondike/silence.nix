940be4ccbcb12f2e9e724630b54cb002a7019703
Alessandro Candido <candido.ale@gmail.com>
2024-03-13 13:50:33 +0100

    feat: Silence klondike every minute

---
+++ systems/nixos/klondike/silence.nix
@@ -0,0 +1,12 @@
+# Shut down to avoid a noisy machine running during office hours
+{
+  systemd.services.shutdown = {
+    enable = true;
+    wantedBy = ["multi-user.target"];
+    description = "Shut down klondike during office hours.";
+    serviceConfig = {
+      ExecStart = "sudo shutdown now";
+    };
+    startAt = "*:00/01:00"; # every minute
+  };
+}
