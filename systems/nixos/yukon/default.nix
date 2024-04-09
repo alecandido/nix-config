f7818ac8d91f65d9aa30ee5a16f23504886fbf34
Alessandro Candido <candido.ale@gmail.com>
2024-04-10 01:40:27 +0200

    fix: Fix invalid symbols

--- systems/nixos/yukon/default.nix
+++ systems/nixos/yukon/default.nix
@@ -27,21 +27,33 @@
               )
 
               (deflayer symbols
-                esc  _    _    _    _    _    ^    &    _    _    _    _    _    _
-                \    !    @    {    }    |    home pgdn pgup end  _    _    _    _
-                esc  #    $    (    )    `    left down up   rght _    '    _
-                lsft %    @usc *    =    ~    +    -    ,    .    /    rsft
+                _    _    _    _    _    _    _    _    _    _    _    _    _    _
+                \    @bng @at  @car @and @pip home pgdn pgup end  _    _    _    _
+                _    @pnd @dol @lpr @rpr `    left down up   rght _    '    _
+                _    @prc @usc @str =    @til +    -    _    _    _    _
               )
 
               (deflayer mouse
-                esc  _    _    _    _    _    _    _    _    _    _    _    _    _
+                _    _    _    _    _    _    _    _    _    _    _    _    _    _
                 _    _    _    @msu _    _    @mwl @mwd @mwu @mwr pp   _    _    _
-                esc  _    @msl @msd @msr _    _    mlft mrgt mmid prev next _
-                lsft _    _    _    _    _    _    _    _    _    _    rsft
+                _    _    @msl @msd @msr _    _    mlft mrgt mmid prev next _
+                _    _    _    _    _    _    _    _    _    _    _    _
               )
 
               (defalias
                 usc (multi lsft -)
+                bng (multi lsft 1)
+                at  (multi lsft 2)
+                pnd (multi lsft 3)
+                dol (multi lsft 4)
+                prc (multi lsft 5)
+                car (multi lsft 6)
+                and (multi lsft 7)
+                str (multi lsft 8)
+                lpr (multi lsft 9)
+                rpr (multi lsft 0)
+                pip (multi lsft \)
+                til (multi lsft `)
 
                 sym (tap-hold-press 200 200 ; (layer-while-held symbols))
                 mou (tap-hold-release 200 200 a (layer-while-held mouse))
