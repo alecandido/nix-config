93f82b1c9ec2f07a7d6912097887db2eeaa94186
Alessandro Candido <candido.ale@gmail.com>
2024-04-12 17:43:53 +0200

    feat: Update gdb configs

--- home/build.nix
+++ home/build.nix
@@ -7,5 +7,19 @@
   xdg.configFile."gdb/gdbinit".text = ''
     set history save
     tui enable
+    # TODO: remove the following
+    set auto-load safe-path /
+
+    define hook-next
+      refresh
+    end
+
+    define hook-step
+      refresh
+    end
+
+    define hook-continue
+      refresh
+    end
   '';
 }
