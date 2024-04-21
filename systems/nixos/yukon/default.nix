c99cca397bbb0896b5e0e5c3b9190406e7b406f3
Alessandro Candido <candido.ale@gmail.com>
2024-04-21 16:54:36 +0200

    feat: Add custom udev rule for microbit development

--- systems/nixos/yukon/default.nix
+++ systems/nixos/yukon/default.nix
@@ -1,4 +1,4 @@
-{
+{lib, ...}: {
   imports = [
     # Include the results of the hardware scan.
     ./hardware-configuration.nix
@@ -7,4 +7,8 @@
   ];
 
   services.xserver.displayManager.gdm.autoSuspend = false;
+  services.udev.extraRules = lib.mkAfter ''
+    # CMSIS-DAP for microbit
+    SUBSYSTEM=="usb", ATTR{idVendor}=="0d28", ATTR{idProduct}=="0204", MODE:="666"
+  '';
 }
