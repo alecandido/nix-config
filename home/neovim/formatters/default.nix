b6d108c3137b3809eabdc56533244dff3973c195
Alessandro Candido <candido.ale@gmail.com>
2025-02-13 00:24:54 +0100

    fix: Drop toggles usage from formatters as well

--- home/neovim/formatters/default.nix
+++ home/neovim/formatters/default.nix
@@ -1,39 +1,39 @@
 {
+  config,
+  lib,
   pkgs,
-  toggles,
   ...
 }: let
-  lsp = builtins.elem "neovim.lsp" toggles;
+  inherit (lib) mkIf;
+  cfg = config.plan.neovim;
   nufmt-in = pkgs.writeScriptBin "nufmt-in" ''
     #!/usr/bin/env -S nu --stdin
     nufmt --stdin $in | tail -n +2
   '';
 in {
-  home.packages =
-    if lsp
-    then
-      (with pkgs;
-        [
-          alejandra
-          black
-          fnlfmt
-          isort
-          ktlint
-          pgformatter
-          rustfmt
-          shfmt
-          stylish-haskell
-          stylua
-          uncrustify
-        ]
-        ++ (lib.optionals (!stdenv.isDarwin) [nufmt nufmt-in])
-        ++ (with nodePackages; [
-          prettier
-        ])
-        ++ (with python311Packages; [
-          docformatter
-        ]))
-    else [];
+  config = mkIf (cfg.enable && cfg.lsp) {
+    home.packages = with pkgs;
+      [
+        alejandra
+        black
+        fnlfmt
+        isort
+        ktlint
+        pgformatter
+        rustfmt
+        shfmt
+        stylish-haskell
+        stylua
+        uncrustify
+      ]
+      ++ (lib.optionals (!stdenv.isDarwin) [nufmt nufmt-in])
+      ++ (with nodePackages; [
+        prettier
+      ])
+      ++ (with python311Packages; [
+        docformatter
+      ]);
 
-  xdg.configFile."uncrustify.cfg".source = ./uncrustify.cfg;
+    xdg.configFile."uncrustify.cfg".source = ./uncrustify.cfg;
+  };
 }
