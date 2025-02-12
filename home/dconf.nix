b81e0557d96b3f8209b039c85af4d68e51586a33
Alessandro Candido <candido.ale@gmail.com>
2025-02-13 00:39:50 +0100

    fix: Erase toggles from dconf
    Final home module!

--- home/dconf.nix
+++ home/dconf.nix
@@ -4,47 +4,54 @@
 # dconf watch /
 # ```
 # and perform the action.
-{toggles, ...}: let
-  gnome = builtins.elem "gnome" toggles;
-  server = builtins.elem "server" toggles;
+{
+  config,
+  lib,
+  ...
+}: let
+  inherit (lib) mkIf mkEnableOption;
+  cfg = config.plan.dconf;
 in {
-  dconf = {
-    enable = true;
-    settings =
-      {}
-      // (
-        if gnome
-        then
-          ({
-              "org/gnome/desktop/interface" = {
-                color-scheme = "prefer-dark";
-              };
-              "org/gnome/mutter" = {
-                dynamic-workspaces = true;
-                edge-tiling = true;
-                workspaces-only-on-primary = true;
-              };
-              "org/gnome/shell" = {
-                enabled-extensions = ["appindicatorsupport@rgcjonas.gmail.com"];
-              };
-              "org/gnome/shell/app-switcher" = {
-                current-workspace-only = true;
-              };
-              "org/gnome/desktop/wm/keybindings" = {
-                switch-windows = ["<Alt>Tab"];
-                switch-windows-backward = ["<Shift><Alt>Tab"];
-              };
-            }
-            // (
-              if server
-              then {
-                "org/gnome/settings-daemon/plugins/power" = {
-                  sleep-inactive-ac-type = "nothing";
-                };
-              }
-              else {}
-            ))
-        else {}
-      );
+  options.plan.dconf = {
+    enable = mkEnableOption "dconf";
+    server = mkEnableOption "dconf on server";
+  };
+
+  config = mkIf cfg.enable {
+    dconf = {
+      enable = true;
+
+      settings = let
+        gui = {
+          "org/gnome/desktop/interface" = {
+            color-scheme = "prefer-dark";
+          };
+          "org/gnome/mutter" = {
+            dynamic-workspaces = true;
+            edge-tiling = true;
+            workspaces-only-on-primary = true;
+          };
+          "org/gnome/shell" = {
+            enabled-extensions = ["appindicatorsupport@rgcjonas.gmail.com"];
+          };
+          "org/gnome/shell/app-switcher" = {
+            current-workspace-only = true;
+          };
+          "org/gnome/desktop/wm/keybindings" = {
+            switch-windows = ["<Alt>Tab"];
+            switch-windows-backward = ["<Shift><Alt>Tab"];
+          };
+        };
+        server =
+          if cfg.server
+          then {
+            "org/gnome/settings-daemon/plugins/power" = {
+              sleep-inactive-ac-type = "nothing";
+            };
+          }
+          else {};
+      in
+        gui // server;
+    };
   };
 }
