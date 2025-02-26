92e0d7e181773b3828e78ae4503adbd60f241f9e
Alessandro Candido <candido.ale@gmail.com>
2025-02-26 10:47:43 +0100

    feat!: Drop autossh on killmotor

--- systems/nixos/PCTH275/default.nix
+++ systems/nixos/PCTH275/default.nix
@@ -2,7 +2,6 @@
   imports = [
     # Include the results of the hardware scan.
     ./hardware-configuration.nix
-    ./networking.nix
   ];
 
   services.xserver.displayManager.gdm.autoSuspend = false;
