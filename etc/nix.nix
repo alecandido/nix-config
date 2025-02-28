c61b5f2556eae35c4147f62b035961449128c667
Alessandro Candido <candido.ale@gmail.com>
2025-02-28 16:16:37 +0100

    fix: Lift options from home-manager to os

--- etc/nix.nix
+++ etc/nix.nix
@@ -1,14 +1,23 @@
 {inputs, ...}: {
-  nix.registry.nixpkgs.flake = inputs.nixpkgs;
+  nix = {
+    registry.nixpkgs.flake = inputs.nixpkgs;
 
-  nix.settings = {
-    substituters = ["https://cache.nixos.org" "https://alecandido.cachix.org"];
-    trusted-substituters = ["https://devenv.cachix.org"];
-    trusted-users = ["root"];
-    trusted-public-keys = [
-      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-      "alecandido.cachix.org-1:TgsrZYrtoZVpZ9nSlYBw2ylVFpHqLI1YnwhPTwjGdgM="
-      "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
-    ];
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 1w";
+    };
+
+    settings = {
+      experimental-features = ["nix-command" "flakes"];
+      substituters = ["https://cache.nixos.org" "https://alecandido.cachix.org"];
+      trusted-substituters = ["https://devenv.cachix.org"];
+      trusted-users = ["root"];
+      trusted-public-keys = [
+        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+        "alecandido.cachix.org-1:TgsrZYrtoZVpZ9nSlYBw2ylVFpHqLI1YnwhPTwjGdgM="
+        "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
+      ];
+    };
   };
 }
--- home/nix.nix
+++ home/nix.nix
@@ -7,17 +7,7 @@
   system = pkgs.system;
   devenv = inputs.devenv.packages.${system}.default;
 in {
-  nix = {
-    settings = {
-      experimental-features = ["nix-command" "flakes"];
-      trusted-users = ["root" user];
-    };
-    gc = {
-      automatic = true;
-      dates = "weekly";
-      options = "--delete-older-than 1w";
-    };
-  };
+  nix.settings.trusted-users = [user];
 
   # Useful nix related tools
   home.packages =
