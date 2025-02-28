8d5419f5ecba2447e962e8b31dc53038e5cae4c5
Alessandro Candido <candido.ale@gmail.com>
2025-02-28 16:23:45 +0100

    fix: Duplicate experimental features declaration in home-manager
    To make sure to have it even in non-nixos nor nix-darwin systems

--- home/nix.nix
+++ home/nix.nix
@@ -7,7 +7,10 @@
   system = pkgs.system;
   devenv = inputs.devenv.packages.${system}.default;
 in {
-  nix.settings.trusted-users = [user];
+  nix.settings = {
+    experimental-features = ["nix-command" "flakes"];
+    trusted-users = [user];
+  };
 
   # Useful nix related tools
   home.packages =
