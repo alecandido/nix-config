7a57180f9050ce9b5e64d52473f644851927f77f
Alessandro Candido <candido.ale@gmail.com>
2025-03-23 18:46:13 +0100

    feat: Add function to identify nix files
    The goal is to eventually use to automatically source these files.

--- lib/default.nix
+++ lib/default.nix
@@ -1,1 +1,19 @@
-{}
+{lib, ...}: {
+  nixDir = path:
+    builtins.map
+    (f: (path + "/${f}"))
+    (builtins.attrNames
+      (lib.attrsets.filterAttrs
+        (
+          path: _type:
+            !(lib.strings.hasPrefix "." path) # exclude hidden paths
+            && (
+              (_type == "directory") # include directories
+              || (
+                (path != "default.nix") # ignore default.nix
+                && (lib.strings.hasSuffix ".nix" path) # include .nix files
+              )
+            )
+        )
+        (builtins.readDir path)));
+}
