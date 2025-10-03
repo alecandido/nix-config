9c992e2ad8d54ae33606c0c9c2bf1ba97139316e
Alessandro Candido <candido.ale@gmail.com>
2025-10-03 15:49:22 +0200

    feat: Introduce remote desktop for yukon

--- systems/nixos/yukon/networking.nix
+++ systems/nixos/yukon/networking.nix
@@ -1,23 +1,10 @@
-{
-  services.openssh = {
-    enable = true;
-    ports = [22 443 2222];
-
-    settings = {
-      PasswordAuthentication = true;
-      AllowUsers = null;
-      UseDns = true;
-      X11Forwarding = false;
-      PermitRootLogin = "prohibit-password";
-    };
-  };
-
+{pkgs, ...}: {
   networking = {
     nftables.enable = true;
 
     firewall = {
       enable = true;
-      allowedTCPPorts = [22 80 443 2222];
+      allowedTCPPorts = [22 80 443 2222 3389];
     };
 
     interfaces = {
@@ -31,4 +18,27 @@
 
     defaultGateway = "192.168.1.254";
   };
+
+  services.openssh = {
+    enable = true;
+    ports = [22 443 2222];
+
+    settings = {
+      PasswordAuthentication = true;
+      AllowUsers = null;
+      UseDns = true;
+      X11Forwarding = false;
+      PermitRootLogin = "prohibit-password";
+    };
+  };
+
+  # RDP
+  services.xrdp.enable = true;
+  services.xrdp.defaultWindowManager = "${pkgs.gnome-session}/bin/gnome-session"; # gnome wayland session
+  services.gnome.gnome-remote-desktop.enable = true;
+  systemd.services.gnome-remote-desktop = {
+    wantedBy = ["graphical.target"]; # for starting the unit automatically at boot
+  };
+  services.displayManager.autoLogin.enable = false;
+  services.getty.autologinUser = null;
 }
