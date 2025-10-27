6d6c99687f185ce020f0849f30e997c7ae3fb01c
Alessandro Candido <candido.ale@gmail.com>
2025-10-27 08:09:25 +0400

    fix: Change dns to 1.1.1.1

--- systems/darwin/donaldville/wg.nix
+++ systems/darwin/donaldville/wg.nix
@@ -11,9 +11,16 @@
         "fd31:bf08:57cb::100/128"
         "192.168.26.100/32"
       ];
-      # use dnscrypt, or proxy dns as described above
-      dns = ["127.0.0.1"];
+
+      dns = ["1.1.1.1"];
+
+      # NOTE: this generates a `/etc/wireguard/wg0.conf` containing an
+      # `Interface.PostUp` entry, which sets the private key from the decrypted file
+      # available at runtime from agenix
+      # To use this file in an external application, replace `Interface.PostUp` with an
+      # `Interface.PrivateKey` entry, with the plain unencrypted private key
       privateKeyFile = config.age.secrets.donaldville-wg-key.path;
+
       peers = [
         {
           publicKey = "cF6ZiBFPmN/CMGXmXGC8bdo75m2GKM1xkKOJQemminU=";
