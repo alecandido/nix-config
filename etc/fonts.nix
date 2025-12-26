1e983fe2553a665fd12d34714ea7d21173b4df41
Alessandro Candido <candido.ale@gmail.com>
2025-12-26 19:12:57 +0100

    fix: Improve terminal appearance on killmotor

--- etc/fonts.nix
+++ etc/fonts.nix
@@ -4,8 +4,9 @@
     noto-fonts-cjk-sans
     noto-fonts-cjk-serif
     noto-fonts-emoji
-    nerd-fonts.fira-code
     nerd-fonts.blex-mono
+    nerd-fonts.fira-code
+    nerd-fonts.lilex
     meslo-lgs-nf
     mplus-outline-fonts.githubRelease
   ];
--- systems/nixos/killmotor/home/tools.nix
+++ systems/nixos/killmotor/home/tools.nix
@@ -1,3 +1,9 @@
 {lib, ...}: {
   programs.kitty.font.size = lib.mkForce 12;
+  programs.ghostty.settings = {
+    font-size = lib.mkForce 15;
+    fullscreen = true;
+    font-family = lib.mkForce "FiraCode Nerd Font";
+    theme = lib.mkForce "dark:Catppuccin Mocha,light:Alabaster";
+  };
 }
