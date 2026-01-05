36d17054cf6315346a1b38b9a59e0bc04d93945d
Alessandro Candido <candido.ale@gmail.com>
2026-01-05 16:16:44 +0100

    feat: Add zellij layouts

--- home/zellij/default.nix
+++ home/zellij/default.nix
@@ -1,5 +1,16 @@
-{
+{lib, ...}: let
+  localFiles = lib.filesystem.listFilesRecursive ./.;
+  configPaths = lib.filter (lib.hasSuffix ".kdl") localFiles;
+  pathToEntry = path: (
+    let
+      relPath = lib.path.removePrefix ./. path;
+      name = lib.removePrefix "./" relPath;
+    in
+      lib.attrsets.nameValuePair "zellij/${name}" {source = path;}
+  );
+  configFiles = builtins.listToAttrs (builtins.map pathToEntry configPaths);
+in {
   programs.zellij.enable = true;
 
-  xdg.configFile."zellij/config.kdl".source = ./config.kdl;
+  xdg.configFile = configFiles;
 }
