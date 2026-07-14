c933cd18ec6785b7040c9691f873823b9dd722d3
Alessandro Candido <candido.ale@gmail.com>
2026-07-14 13:15:01 +0400

    fix: Patch devenv missing autoload hook

--- home/nix.nix
+++ home/nix.nix
@@ -1,6 +1,8 @@
 {
+  config,
   pkgs,
   inputs,
+  lib,
   ...
 }: let
   system = pkgs.stdenv.hostPlatform.system;
@@ -17,4 +19,15 @@ in {
       niv
     ])
     ++ [devenv];
+
+  home.activation = let
+    autoload = config.xdg.configHome + "/nushell/autoload";
+  in {
+    # FIXME: remove as soon as devenv will start doing this autonomously
+    devenvNuAutoload = lib.hm.dag.entryAfter ["writeBoundary"] ''
+      export PATH="${pkgs.nushell}/bin:${pkgs.devenv}/bin:$PATH"
+      run mkdir ${autoload}
+      run nu -c 'devenv hook nu | save --force ${autoload}/devenv-hook.nu'
+    '';
+  };
 }
