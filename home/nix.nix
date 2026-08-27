c749ed5db47912da3fcd22897180386d92ced0bb
Alessandro Candido <candido.ale@gmail.com>
2026-08-27 19:01:15 +0400

    fix: Replace devenv installation with hm module

--- home/nix.nix
+++ home/nix.nix
@@ -1,33 +1,15 @@
-{
-  config,
-  pkgs,
-  inputs,
-  lib,
-  ...
-}: let
-  system = pkgs.stdenv.hostPlatform.system;
-  devenv = inputs.devenv.packages.${system}.default;
-in {
+{pkgs, ...}: {
   nix.settings = {
     experimental-features = ["nix-command" "flakes"];
   };
 
   # Useful nix related tools
-  home.packages =
-    (with pkgs; [
-      cachix
-      niv
-    ])
-    ++ [devenv];
+  home.packages = with pkgs; [
+    cachix
+  ];
 
-  home.activation = let
-    autoload = config.xdg.configHome + "/nushell/autoload";
-  in {
-    # FIXME: remove as soon as devenv will start doing this autonomously
-    devenvNuAutoload = lib.hm.dag.entryAfter ["writeBoundary"] ''
-      export PATH="${pkgs.nushell}/bin:${pkgs.devenv}/bin:$PATH"
-      run mkdir -p ${autoload}
-      run nu -c 'devenv hook nu | save --force ${autoload}/devenv-hook.nu'
-    '';
+  programs.devenv = {
+    enable = true;
+    enableNushellIntegration = true;
   };
 }
