541d78c19b2768aa08c99347880d618744f98cce
Alessandro Candido <candido.ale@gmail.com>
2026-09-08 18:05:13 +0400

    feat: Add bluesnooze to darwin

--- etc/darwin/default.nix
+++ etc/darwin/default.nix
@@ -1,4 +1,4 @@
-{pkgs, ...}: {
+{ pkgs, ... }: {
   imports = [
     ../fonts.nix
     ../nix.nix
@@ -9,18 +9,23 @@
   # Add ability to used TouchID for sudo authentication
   security.pam.services.sudo_local.touchIdAuth = true;
 
-  environment = let
-    shell = "${pkgs.nushell}/bin/nu";
-  in {
-    pathsToLink = ["/share/zsh"];
-    shells = with pkgs; [nushell];
-    shellAliases = {
-      upgrade = ''
-        sudo darwin-rebuild switch --flake "$HOME/Projects/nix-config" --show-trace
-      '';
+  environment =
+    let
+      shell = "${pkgs.nushell}/bin/nu";
+    in
+    {
+      pathsToLink = [ "/share/zsh" ];
+      shells = with pkgs; [ nushell ];
+      shellAliases = {
+        upgrade = ''
+          sudo darwin-rebuild switch --flake "$HOME/Projects/nix-config" --show-trace
+        '';
+      };
+      variables = {
+        SHELL = shell;
+      };
+      defaultPackages = with pkgs; [ bluesnooze ];
     };
-    variables = {SHELL = shell;};
-  };
 
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
