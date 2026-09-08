0098b4b77ab886fe4d1a5905dfb9a0f2a1969ff5
Alessandro Candido <candido.ale@gmail.com>
2026-09-08 18:25:13 +0400

    fix: Comment out gemini token, authenticate interactively

--- home/ai.nix
+++ home/ai.nix
@@ -4,10 +4,11 @@
   pkgs,
   lib,
   ...
-}: let
+}:
+let
   token = config.age.secrets.llm-qrc-token.path;
   url = config.age.secrets.llm-qrc-url.path;
-  geminiToken = config.age.secrets.gemini-token.path;
+  # geminiToken = config.age.secrets.gemini-token.path;
 
   # copilot using the qwen/qrc backend
   copilotQrc = pkgs.writeShellScriptBin "copilot-qrc" ''
@@ -18,28 +19,31 @@
     exec copilot "$@"
   '';
 
-  trimFirst = x: builtins.substring 1 (builtins.stringLength x - 1) x;
+  # trimFirst = x: builtins.substring 1 (builtins.stringLength x - 1) x;
 
   agySettings = {
     modelProvider = "gemini";
     colorScheme = "terminal";
   };
-in {
+in
+{
   programs.github-copilot-cli = {
     enable = true;
   };
 
-  home.activation.updateAntigravitySettings = let
-    settings = config.home.homeDirectory + "/.gemini/antigravity-cli/settings.json";
-    jsonUpdate = builtins.toJSON agySettings;
-    tmp = "/tmp/hm-activate-antigravity-settings";
-  in (lib.hm.dag.entryAfter ["writeBoundary" "linkGeneration"] ''
-    if [ ! -s "${settings}" ]; then
-        echo "{}" > ${settings}
-    fi
-    ${lib.getExe pkgs.jq} -s '.[0] * .[1]' ${settings} <(echo '${jsonUpdate}') > ${tmp}
-    mv ${tmp} ${settings}
-  '');
+  home.activation.updateAntigravitySettings =
+    let
+      settings = config.home.homeDirectory + "/.gemini/antigravity-cli/settings.json";
+      jsonUpdate = builtins.toJSON agySettings;
+      tmp = "/tmp/hm-activate-antigravity-settings";
+    in
+    (lib.hm.dag.entryAfter [ "writeBoundary" "linkGeneration" ] ''
+      if [ ! -s "${settings}" ]; then
+          echo "{}" > ${settings}
+      fi
+      ${lib.getExe pkgs.jq} -s '.[0] * .[1]' ${settings} <(echo '${jsonUpdate}') > ${tmp}
+      mv ${tmp} ${settings}
+    '');
 
   age.secrets.llm-qrc-url.file = inputs.secrets.llm-qrc-url;
   age.secrets.llm-qrc-token.file = inputs.secrets.llm-qrc-token;
@@ -50,8 +54,8 @@ in {
     copilotQrc
   ];
 
-  programs.nushell.extraEnv = ''
-    # trim the first character for both, since it is intended for Bash, and it is a $
-    $env.GEMINI_API_KEY = (open --raw ${trimFirst geminiToken});
-  '';
+  # programs.nushell.extraEnv = ''
+  #   # trim the first character for both, since it is intended for Bash, and it is a $
+  #   $env.GEMINI_API_KEY = (open --raw ${trimFirst geminiToken});
+  # '';
 }
